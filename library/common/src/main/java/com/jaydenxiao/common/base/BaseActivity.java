8be8259f123c7694b9f06b6db7b58dc586cd49e3
wangfeng19930909 <1300497680@qq.com>
2018-11-28 11:18:42 +0800

    修改app主色调

--- library/common/src/main/java/com/jaydenxiao/common/base/BaseActivity.java
+++ library/common/src/main/java/com/jaydenxiao/common/base/BaseActivity.java
@@ -135,7 +135,7 @@ public abstract class BaseActivity<T extends BasePresenter, E extends BaseModel>
      * 着色状态栏（4.4以上系统有效）
      */
     protected void SetStatusBarColor(){
-        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this,R.color.statusbar_color));
+        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this,R.color.main_color));
     }
     /**
      * 着色状态栏（4.4以上系统有效）
